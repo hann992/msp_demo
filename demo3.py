1e2a1f5617e0e9a532cebb7aacc2de71a4eb911d
Andreas <79266289+hann992@users.noreply.github.com>
2025-02-21 01:30:55 +0100

    Initial commit

---
+++ demo3.py
@@ -0,0 +1,20 @@
+
+
+
+myNumber = 3
+myString = "Hello, World!"
+myList = [1, 2, 3, 4, 5]
+myTuple = (1, 2, 3, 4, 5)
+myDictionary = {"name": "John", "age": 30}
+mySet = {1, 2, 3, 4, 5}
+myBoolean = True
+myNone = None
+
+print(myNumber)
+print(myString)
+print(myList)
+print(myTuple)
+print(myDictionary)
+print(mySet)
+print(myBoolean)
+
